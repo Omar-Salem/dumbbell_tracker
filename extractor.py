1fca65dd4103155997dfd1b5ed92f1c56dd33678
Omar Salem <omar.salem@etika.com>
2024-06-06 11:14:10 +1000

    refactor

--- extractor.py
+++ extractor.py
@@ -12,17 +12,22 @@ class Extractor:
         self.kwargs={"conf":.5}
         self.model = YOLO("yolov8n.pt")
 
-    def __str__(self):
-        return f'{self.x}, {self.y}'
-
-    def __repr__(self):
-        return self.__str__()
+        self.backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe']
+        self.db_path="db"
 
     def __extractXY__(self,b):
         xyxy=b.xyxy[0]
         return int(xyxy[0].item()),int(xyxy[1].item()),int(xyxy[2].item()),int(xyxy[3].item())
+    
+    def __identify__(self,frame):
+        result = DeepFace.find(img_path = frame, db_path = self.db_path, detector_backend=self.backends[2])
+        for row in result:
+            identity = row["identity"][0]
+            fileNameWithExtensions=os.path.basename(identity)
+            personName=os.path.splitext(fileNameWithExtensions)[0]
+            return
 
-    def findPersonClosestToPoint(self,frame:numpy.ndarray,q:list):
+    def findPersonClosestToPoint(self, frame:numpy.ndarray, q:list):
         results = self.model.predict(source=frame, **self.kwargs)  # Display preds. Accepts all YOLO predict arguments
         boxes=results[0].boxes
         persons=[b for b in boxes if b.cls.item()==0]
@@ -33,23 +38,15 @@ class Extractor:
             x1=c[0]
             y1=c[1]
             d=math.dist([x1,y1], q)
-            if d<minDistance:
+            if minDistance>d:
                 minDistance=d
                 xyxy=c
         
         x1=xyxy[0]
         y1=xyxy[1]
-        
+
         x2=xyxy[2]
         y2=xyxy[3]
-        frame = frame[y1:y2, x1:x2]  #TODO refactor
-        # im = Image.fromarray(frame)
-        # im.save('_0.png')
-        backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe']
-        db_path="db"
-        result = DeepFace.find(img_path = frame, db_path = db_path, detector_backend=backends[2])
-        for row in result:
-            identity = row["identity"][0]
-            fileNameWithExtensions=os.path.basename(identity)
-            personName=os.path.splitext(fileNameWithExtensions)[0]
-            return
+
+        cropped = frame[y1:y2, x1:x2]
+        return self.__identify__(cropped)
