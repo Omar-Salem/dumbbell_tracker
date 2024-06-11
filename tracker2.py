af92271bfa8efc7630261e048dcc47dca0ea27ab
Omar Salem <omar.salem@etika.com>
2024-06-11 19:16:17 +1000

    position matching

--- tracker2.py
+++ tracker2.py
@@ -11,20 +11,90 @@
 # cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,255), 5)
 
 # plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
+################################
 import cv2
 import numpy as np
+import pathlib
 
-img_rgb = cv2.imread('s2.png')
-template = cv2.imread('o.png')
-w, h = template.shape[:-1]
-
-res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
-threshold = .8
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):  # Switch columns and rows
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+img_rgb = cv2.imread('image.png')
+for d in pathlib.Path('./dumbells').glob('*.png'):
+	print(d)
+	template = cv2.imread(str(d))
+	w, h = template.shape[:-1]
+	res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+	threshold = .9
+	loc = np.where(res >= threshold)
+	for pt in zip(*loc[::-1]):  # Switch columns and rows
+		cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
 cv2.imwrite('result.png', img_rgb)
+# # # cv2.imshow(img_rgb)
 
-# cv2.waitKey(0)
+# ###################################
+# import cv2 
+# import numpy as np 
+# from matplotlib import pyplot as plt 
+  
+# # reading image 
+# img = cv2.imread('shapes.png') 
+  
+# # converting image into grayscale image 
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+  
+# # setting threshold of gray image 
+# _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY) 
+  
+# # using a findContours() function 
+# contours, _ = cv2.findContours( 
+#     threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+  
+# i = 0
+  
+# # list for storing names of shapes 
+# for contour in contours: 
+  
+#     # here we are ignoring first counter because  
+#     # findcontour function detects whole image as shape 
+#     if i == 0: 
+#         i = 1
+#         continue
+  
+#     # cv2.approxPloyDP() function to approximate the shape 
+#     approx = cv2.approxPolyDP( 
+#         contour, 0.01 * cv2.arcLength(contour, True), True) 
+      
+#     # using drawContours() function 
+#     cv2.drawContours(img, [contour], 0, (0, 0, 255), 5) 
+  
+#     # finding center point of shape 
+#     M = cv2.moments(contour) 
+#     if M['m00'] != 0.0: 
+#         x = int(M['m10']/M['m00']) 
+#         y = int(M['m01']/M['m00']) 
+  
+#     # putting shape name at center of each shape 
+#     if len(approx) == 3: 
+#         cv2.putText(img, 'Triangle', (x, y), 
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+  
+#     elif len(approx) == 4: 
+#         cv2.putText(img, 'Quadrilateral', (x, y), 
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+  
+#     elif len(approx) == 5: 
+#         cv2.putText(img, 'Pentagon', (x, y), 
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+  
+#     elif len(approx) == 6: 
+#         cv2.putText(img, 'Hexagon', (x, y), 
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+  
+#     else: 
+#         cv2.putText(img, 'circle', (x, y), 
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+  
+# # displaying the image after drawing contours 
+# cv2.imshow('shapes', img) 
+  
+# cv2.waitKey(0) 
+# cv2.destroyAllWindows()
